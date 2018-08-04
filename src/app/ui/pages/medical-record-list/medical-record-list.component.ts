2d26ba6361968f55dfe83e6ca38e1b82d8711429
Batman @Moshi-OSX <aobphilos@gmail.com>
2018-08-04 19:27:53 +0700

    update Medical Record List

--- src/app/ui/pages/medical-record-list/medical-record-list.component.ts
+++ src/app/ui/pages/medical-record-list/medical-record-list.component.ts
@@ -1,13 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons, NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-medical-record-list',
   templateUrl: './medical-record-list.component.html',
   styleUrls: ['./medical-record-list.component.scss']
 })
+
 export class MedicalRecordListComponent implements OnInit {
 
-  constructor() { }
+  model: {
+    cid: string,
+    chronic: string,
+    drugAllergyDate: NgbDateStruct,
+    checkedDate: Date
+  };
+
+  constructor(
+    private modalService: NgbModal,
+    private calendar: NgbCalendar
+  ) {
+    this.resetModel();
+  }
+
+  private resetModel() {
+    this.model = {
+      cid: '',
+      chronic: '',
+      drugAllergyDate: this.calendar.getToday(),
+      checkedDate: new Date()
+    };
+  }
+  openRequest(content, mode = 'new') {
+    this.modalService
+      .open(content, {
+        ariaLabelledBy: mode === 'new' ? 'addMedicalLabel' : 'editMedicalLabel',
+        backdrop: 'static'
+      })
+      .result.then((result) => {
+        this.resetModel();
+      }, (reason) => {
+        this.resetModel();
+      });
+  }
 
   ngOnInit() {
   }
